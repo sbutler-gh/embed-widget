093d5a0a333d988fbd55a7fc97a3565b7baf0a6e
sbutler-gh <sam@ysli.be>
2022-06-09 19:05:47 +0200

    Made simplified widget design responsive on the forms

--- src/routes/Embed.svelte
+++ src/routes/Embed.svelte
@@ -99,38 +99,38 @@ let data = [];
     <div id="banner">
 
         {#if !submit_results}
-        <form class="dark" on:submit={submitForm}>
-
-
-            <div style="text-align: center">
+        <div class="lead" style="text-align: center">
             <h3>Ready for <span style="color: palegreen; font-weight: bold">climate action</span>?</h3>
-            <p>Add your details below, and we'll connect you with local groups and drive action together.</p>
+            <p style="">Connect with local climate groups, and help drive collective action.</p>
             <!-- <p>It's time we take the lead.  Add your details below, and we'll help you empower collective action.</p> -->
             </div>
 
+        <form class="responsive" on:submit={submitForm}>
+
             <div class="flex-row" style="margin: auto; width: fit-content;">
-                <div>
+                <div class="form-item">
                     <label for="email">Email</label>
-                    <input style="width: 150px; font-size: 16px; margin-top: 5px;" type="email" name="email" placeholder="" required>
+                    <input style="" type="email" name="email" placeholder="" required>
                 </div>
-                <div>
+                <div class="form-item">
                     <label for="address">Address</label>
-                    <input style="width: 200px; font-size: 16px; margin-top: 5px;" type="text" name="address" required>
+                    <input id="address_input" style="" type="text" name="address" placeholder="123 Main Street, New York, NY, 12345" required>
                     <!-- <input style="width: 60px;" type="text" name="postal" bind:value={postal} placeholder="" required> -->
                     </div>
-                    <div>
+                    <div class="form-item">
                         <label for="interests">Describe your interests and skills</label>
-                        <textarea style="width: 300px; height: 21.5px;" name="interests"></textarea>
+                        <textarea style="width: 300px;" name="interests"></textarea>
                         <!-- <input style="width: 300px; font-size: 16px; margin-top: 5px;" type="text" name="interests" required> -->
                     </div>
             </div>
-            <div>
+            <div class="form-item">
                 <button class="greenbutton">Submit</button>
                 </div>
             </form>
 
                 {:else}
                 <p><em>Perfect!  Now look out for an email connecting you to local climate action groups in your area, and more information about leading real change.</em></p>
+                <p>(This is a demo of what the form submission could look like — there isn't an email being sent at this time.)</p>
                 <p><strong>Email</strong>: {submit_results.email}</p>
                 <p><strong>Address</strong>: {submit_results.address}</p>
                 <p><strong>Interests and skills</strong>: {submit_results.interests}</p>
@@ -165,20 +165,6 @@ let data = [];
 
     <style>
 
-        .dark input, .dark textarea {
-            background: none;
-            color: white;
-            border: none;
-            outline: none;
-            border-bottom: solid 1px white;
-        }
-
-        .dark input:focus, .dark textarea:focus {
-            border-color: #a4d2ff;
-        box-shadow: 0 0 6px #1b6ac97f;
-        outline: none;
-        }
-
     #banner {
         overflow-y: scroll;
         border: solid 1px black; 
@@ -193,12 +179,123 @@ let data = [];
             max-width: fit-content;
             max-height: 300px;
      }
+
+     .lead {
+         max-width: 75%;
+         margin: auto;
+     }
+
+     .responsive .flex-row {
+         display: flex;
+     }
+
+     .flex-row .form-item {
+        margin-left: 20px;
+    }
+
+     .form-item input {
+        font-size: 16px; margin-top: 5px;
+     }
+
+     .form-item textarea {
+        height: 21.5px;
+        width: 300px;
+     }
+
+     #address_input {
+         width: 200px;
+     }
+
+     .responsive input, .responsive textarea {
+            background: none;
+            color: white;
+            border: none;
+            outline: none;
+            border-bottom: solid 1px white;
+        }
+
+        .responsive input:focus, .responsive textarea:focus {
+            border-color: #a4d2ff;
+        box-shadow: 0 0 6px #1b6ac97f;
+        outline: none;
+        }
+
+     
     }
     @media only screen and (max-width: 600px) {
         #banner {
             max-width: 100%;
             max-height: 200px;
      }
+
+     .responsive {
+    border-radius: 10px;
+    padding: 10px;
+    /* box-shadow: 0 -1px 10px #0000000d, 0 1px 4px #0000001a, 0 10px 30px #f3ece8; */
+    background: white;
+    border: solid 2px lightblue;
+    color: black;
+    text-align: left !important;
+    max-width: 300px;
+    margin: auto;
+    }
+
+    .responsive input:not(.range_input) {
+        border: solid lightgrey 1px;
+        border-radius: 5px;
+        padding: 0.5rem;
+        font-size: 16px;
+        max-width: 75%;
+    }
+
+    .responsive textarea {
+        border: solid lightgrey 1px;
+        border-radius: 5px;
+        padding: 0.25rem;
+        font-size: 14px;
+        max-width: 100%; 
+    }
+
+    #address_input {
+         max-width: 90%;
+         width: 300px;
+     }
+
+    .range_input {
+        background: none;
+        border: none;
+        border-bottom: solid 1px #f0f0f0;
+        color: black;
+        width: 25px;
+        display: inline;
+        font-size: 16px;
+        padding: 0.5rem;
+        outline: none;
+    }
+
+    .responsive input:focus:not(.range_input), .responsive textarea:focus {
+        border-color: #a4d2ff;
+        box-shadow: 0 0 6px #1b6ac97f;
+        outline: none;
+    }
+
+    .responsive button {
+        border-radius: 20px;
+    color: #fff;
+    border: none;
+    background: #2da562;
+    padding: 0.5em 2em;
+    margin-top: 10px;
+    font-size: 16px;
+    cursor: pointer;
+    margin-left: auto;
+    display: block;
+    }
+
+    /* .form-item {
+        max-width: 90%;
+    } */
+
     }
 
     form label, form p, form select, form input {
@@ -233,13 +330,9 @@ let data = [];
         margin-left: 5px;
     }
 
-    .flex-row {
+    /* .flex-row {
         display: flex;
-    }
-
-    .flex-row div:not(:first-of-type) {
-        margin-left: 10px;
-    }
+    } */
 
     .light {
     border-radius: 10px;
